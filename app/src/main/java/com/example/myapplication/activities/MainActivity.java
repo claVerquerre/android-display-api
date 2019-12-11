e9ba8b1787eebf7244d73ab9db0ab934db27f574
Verquerre Clara <verquerre@b11p3.fil.univ-lille1.fr>
2019-12-11 16:34:13 +0100

    fix main activity

--- app/src/main/java/com/example/myapplication/activities/MainActivity.java
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.java
@@ -6,18 +6,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.MyApplication;
 import com.example.myapplication.R;
 import com.example.myapplication.adapters.RecyclerViewAdapter;
 import com.example.myapplication.model.Hero;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -25,69 +24,66 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
 
-    private final String BASE_URL = "https://superheroapi.com/api/" + MyApplication.API_KEY;
-    private JsonArrayRequest request;
-    private RequestQueue requestQueue;
+    private final String BASE_URL = "https://superheroapi.com/api/" + MyApplication.API_KEY + "/";
+    private JsonObjectRequest request;
     private List<Hero> heroList;
     private RecyclerView recyclerView;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        heroList = new ArrayList<>();
         recyclerView = findViewById(R.id.recyclerView);
+        heroList = new ArrayList<>();
+
         jsonRequest();
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, heroList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
+    /**
+     *
+     */
     private void jsonRequest() {
-        request = new JsonArrayRequest(BASE_URL, new Response.Listener<JSONArray>() {
 
-            @Override
-            public void onResponse(JSONArray response) {
+        for (int i=1; i<=10; i++){
 
-                JSONObject jsonObject;
+            String url = BASE_URL + i;
+            request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
 
-                for (int i = 0; i<response.length(); i++) {
-                    try {
-                        jsonObject = response.getJSONObject(i);
-
+                    // try {
                         // get all information about a hero from json
                         Hero hero = new Hero();
-                        hero.setName(jsonObject.getString("name"));
-                        hero.setPowerstats(jsonObject.getString("powerstats"));
-                        hero.setBiography(jsonObject.getString("biography"));
-                        hero.setAppearance(jsonObject.getString("appearance"));
-                        hero.setWork(jsonObject.getString("work"));
-                        hero.setImage(jsonObject.getString("image"));
+                        hero.setName("test"); // response.getString("name")
+                        hero.setPowerstats("test"); // response.getString("powerstats")
+                        hero.setBiography("test"); // response.getString("biography")
+                        hero.setAppearance("test"); // response.getString("appearance")
+                        hero.setWork("test"); // response.getString("work")
+                        hero.setImage("test"); // response.getString("image")
 
                         heroList.add(hero);
-                    }
-                    catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                    // }
+                    // catch (JSONException e) {
+                        // e.printStackTrace();
+                    // }
                 }
+            }, new Response.ErrorListener() {
 
-                setupRecyclerView(heroList);
-            }
+                @Override
+                public void onErrorResponse(VolleyError error) {
 
-        }, new Response.ErrorListener() {
+                }
+            });
 
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-
-        requestQueue = Volley.newRequestQueue(MainActivity.this);
-        requestQueue.add(request);
+            requestQueue = Volley.newRequestQueue(MainActivity.this);
+            requestQueue.add(request);
+        }
     }
-
-    private void setupRecyclerView(List<Hero> heroList) {
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, heroList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-    }
-
 }
